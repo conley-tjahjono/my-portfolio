31f0b2b6dcb139b80bf6ce960f59f9c0e778fdd0
Conley Tjahjono <conleytjahjono@gmail.com>
2025-04-27 13:36:51 -0400

    Navbar hover moving

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
       <div className="max-w-xl md:max-w-6xl mx-auto flex justify-between items-center px-7 md:px-10 py-4 lg:py-10">
         <h1 className="font-bold text-lg lg:text-3xl">Conley T.</h1>
         <div className="flex gap-4 md:gap-8 text-sm font-normal lg:text-base">
-          <a href="#projects" className="hover:text-[theme(colors.secondary)] transition-colors">Projects</a>
-          <a href="#skills" className="hover:text-[theme(colors.secondary)] transition-colors">Skills</a>
-          <a href="#contact" className="hover:text-[theme(colors.secondary)] transition-colors">Contact</a>
+          <a href="#projects" className="hover:text-[theme(colors.secondary)] transition-colors duration-300 ease-in-out">Projects</a>
+          <a href="#skills" className="hover:text-[theme(colors.secondary)] transition-colors duration-300 ease-in-out">Skills</a>
+          <a href="#contact" className="hover:text-[theme(colors.secondary)] transition-colors duration-300 ease-in-out">Contact</a>
         </div>
       </div>
     </nav>
