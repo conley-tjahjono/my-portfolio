c77b2dfb34e34b1d1d9a5176f38ad6bf82abe7ff
Conley Tjahjono <conleytjahjono@gmail.com>
2025-04-30 23:37:09 -0400

    Moving Resume as the first

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,6 +1,6 @@
 export const contactSocials = [
+    { label: "resume", href: "/resume.pdf" },
     { label: "github", href: "https://github.com/conley-tjahjono" },
     { label: "linkedin", href: "https://linkedin.com/in/conley-tjahjono" },
     { label: "instagram", href: "https://instagram.com/_conley__" },
-    { label: "resume", href: "/resume.pdf" },
   ];
